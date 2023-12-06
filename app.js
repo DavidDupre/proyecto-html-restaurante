326b8e9c478541e5d15852d661101e69a1e0ad42
TheDarkness1 <132030334+TheDarkness1@users.noreply.github.com>
2023-12-05 21:15:06 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -18,37 +18,37 @@ let products = [
         id: 1,
         name: 'Plato 1',
         image: '1.PNG',
-        price: 120000
+        price: '$120.000'
     },
     {
         id: 2,
         name: 'Plato 2',
         image: '2.PNG',
-        price: 120000
+        price: '$120.000'
     },
     {
         id: 3,
         name: 'Plato 3',
         image: '3.PNG',
-        price: 220000
+        price: '$220.000'
     },
     {
         id: 4,
         name: 'Plato 4',
         image: '4.PNG',
-        price: 123000
+        price: '$123.000'
     },
     {
         id: 5,
         name: 'Plato 5',
         image: '5.PNG',
-        price: 320000
+        price: '$320.000'
     },
     {
         id: 6,
         name: 'Plato 7',
         image: '6.PNG',
-        price: 120000
+        price: '$120.000'
     }
 ];
 let listCards  = [];
@@ -104,4 +104,43 @@ function changeQuantity(key, quantity){
         listCards[key].price = quantity * products[key].price;
     }
     reloadCard();
+}
+document.addEventListener('DOMContentLoaded', function () {
+    checkLoginStatus();
+});
+
+function checkLoginStatus() {
+    if (localStorage.getItem('logged-in')) {
+        showLoggedInState();
+    } else {
+        showLoggedOutState();
+    }
+}
+
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if (username !== '' && password !== '') {
+        localStorage.setItem('logged-in', true);
+        showLoggedInState();
+    } else {
+        alert('Por favor, ingrese un nombre de usuario y contraseña.');
+    }
+}
+
+function logout() {
+    localStorage.removeItem('logged-in');
+    showLoggedOutState();
+}
+
+function showLoggedInState() {
+    document.getElementById('login-container').style.display = 'none';
+    document.querySelector('.container').style.display = 'block';
+    document.body.classList.add('logged-in');
+}
+
+function showLoggedOutState() {
+    document.getElementById('login-container').style.display = 'flex';
+    document.querySelector('.container').style.display = 'none';
+    document.body.classList.remove('logged-in');
 }
